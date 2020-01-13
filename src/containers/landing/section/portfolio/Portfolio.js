28aac701d0b3c45969851bba4ea614b1928bd5a9
Ray Azrin Karim <rayazrink@gmail.com>
2020-01-13 20:45:54 +0700

    change private attr to isPrivate

--- src/containers/landing/section/portfolio/Portfolio.js
+++ src/containers/landing/section/portfolio/Portfolio.js
@@ -12,7 +12,7 @@ const Portfolio = () => {
             id: 1,
             title: 'Haer',
             description: 'Currently developing Human Resource Management System platform\'s front-end. Built using ReactJS',
-            private: false,
+            isPrivate: false,
             url: 'https://haer.co.id',
             imageUrl: 'https://i.ibb.co/HYTg1VT/haer.png'
         },
@@ -20,7 +20,7 @@ const Portfolio = () => {
             id: 2,
             title: 'Tokopedia-UI AI Center Fasilkom UI',
             description: 'Developed website front-end consist of company profile, blog, and board member page. Built using Django',
-            private: false,
+            isPrivate: false,
             url: 'https://tokopedia-ai.cs.ui.ac.id',
             imageUrl: 'https://i.ibb.co/xshHgJZ/tokpedai.png'
         },
@@ -28,7 +28,7 @@ const Portfolio = () => {
             id: 3,
             title: 'BPKP E-Risks',
             description: 'Developed Risk Analytics platform\'s, both its back-end and front-end. Built using Laravel and MySQL',
-            private: true,
+            isPrivate: true,
             url: '',
             imageUrl: 'https://i.ibb.co/3r0KvyM/bpkp.png'
         },
@@ -36,7 +36,7 @@ const Portfolio = () => {
             id: 4,
             title: 'Personal Website',
             description: 'Developed this website from scratch. Built using Figma for design, ReactJS for front-end, and Django for back-end',
-            private: false,
+            isPrivate: false,
             url: 'https://tokopedia-ai.cs.ui.ac.id',
             imageUrl: 'https://i.ibb.co/StCnv0z/thisweb.png'
         },
@@ -44,7 +44,7 @@ const Portfolio = () => {
             id: 5,
             title: 'Penni',
             description: 'Designed and developed whole website front-end consist of landing, register, profile, and marketplace. Built using Django',
-            private: false,
+            isPrivate: false,
             url: 'https://penni.id',
             imageUrl: 'https://i.ibb.co/MphCS2V/penni.png'
         },
@@ -52,7 +52,7 @@ const Portfolio = () => {
             id: 6,
             title: 'BEM FKUI 2019',
             description: 'Developed website both its front-end and back-end. Built using Laravel, MySQL, and Wordpress',
-            private: false,
+            isPrivate: false,
             url: 'https://bem.fk.ui.ac.id',
             imageUrl: 'https://i.ibb.co/wKTsVQ3/bemfk.png'
         },
@@ -60,7 +60,7 @@ const Portfolio = () => {
             id: 7,
             title: 'RS Bhayangkara Lemdiklat Polri',
             description: 'Recreated and developed whole website front-end. Built using Laravel',
-            private: false,
+            isPrivate: false,
             url: 'http://rumkitbhayangkaralemdiklatpolri.com',
             imageUrl: 'https://i.ibb.co/2tF7sd4/rslemdiklat.png'
         },
@@ -68,7 +68,7 @@ const Portfolio = () => {
             id: 8,
             title: 'DTS Indonesia',
             description: 'Develop website front-end consist of profile, article, research, gallery, and FAQ. Build using Laravel',
-            private: false,
+            isPrivate: false,
             url: 'http://dts-indonesia.com',
             imageUrl: 'https://i.ibb.co/yNCtM9C/dts.png'
         },
@@ -76,7 +76,7 @@ const Portfolio = () => {
             id: 9,
             title: 'DailySocial Longform',
             description: 'Developed a page called Longform, research by professional writer packed with interactive and good-looking design',
-            private: false,
+            isPrivate: false,
             url: 'https://dailysocial.id/longform/kecerdasan-buatan-menuju-pilar-ekonomi-indonesia',
             imageUrl: 'https://i.ibb.co/L9ZP8Lc/dailysocial.png'
         },
@@ -153,7 +153,7 @@ const Portfolio = () => {
                                         key={porto.id}
                                         title={porto.title}
                                         description={porto.description}
-                                        private={porto.private}
+                                        private={porto.isPrivate}
                                         url={porto.url}
                                         imageUrl={porto.imageUrl}
                                     />
