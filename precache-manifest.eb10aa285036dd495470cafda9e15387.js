fc5f5be56d7b950ff0ce4efe85c3b7859defbc5e
Ray Azrin Karim <rayazrink@gmail.com>
2020-01-13 09:56:37 +0700

    Updates

---
+++ precache-manifest.eb10aa285036dd495470cafda9e15387.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12d90bd5bd1a6d24bd54cd92823afc0",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "eed12df02e814331ef9f",
+    "url": "/portofolio/static/css/2.529cde92.chunk.css"
+  },
+  {
+    "revision": "13f8f2383badec1176a9",
+    "url": "/portofolio/static/css/main.5d14d9e3.chunk.css"
+  },
+  {
+    "revision": "eed12df02e814331ef9f",
+    "url": "/portofolio/static/js/2.6a7b1f25.chunk.js"
+  },
+  {
+    "revision": "4dbbb2f867497f450e875b8f37375ddf",
+    "url": "/portofolio/static/js/2.6a7b1f25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13f8f2383badec1176a9",
+    "url": "/portofolio/static/js/main.08096bcb.chunk.js"
+  },
+  {
+    "revision": "4f495598075697201307",
+    "url": "/portofolio/static/js/runtime-main.48b0f719.js"
+  },
+  {
+    "revision": "dc6eb0933b2b62c57a7c10102cd81a93",
+    "url": "/portofolio/static/media/blue-line.dc6eb093.svg"
+  },
+  {
+    "revision": "fc7194e80af9f94e9a56bf4825b893a2",
+    "url": "/portofolio/static/media/blue-square.fc7194e8.svg"
+  },
+  {
+    "revision": "abc50fb154e3e88bf2e1bef4ede32801",
+    "url": "/portofolio/static/media/coming-soon.abc50fb1.svg"
+  },
+  {
+    "revision": "194f2ce90a48f21ea1aca6879fa3fd0c",
+    "url": "/portofolio/static/media/email.194f2ce9.svg"
+  },
+  {
+    "revision": "f65f7bbf6d110aceaf097138792fe87e",
+    "url": "/portofolio/static/media/github.f65f7bbf.svg"
+  },
+  {
+    "revision": "a46c2748799f5913ecf16a409b215bbe",
+    "url": "/portofolio/static/media/gitlab.a46c2748.svg"
+  },
+  {
+    "revision": "da4b59b0a50370f31c77b77a248a6c9b",
+    "url": "/portofolio/static/media/instagram.da4b59b0.svg"
+  },
+  {
+    "revision": "76014062fbc01a30aaf3e0f6f3c3c76a",
+    "url": "/portofolio/static/media/linkedin.76014062.svg"
+  },
+  {
+    "revision": "da0e2dd65bfa869aba0c6b6d4447fa16",
+    "url": "/portofolio/static/media/location.da0e2dd6.svg"
+  },
+  {
+    "revision": "126edd1313dbd85ae56efbeb6b14c39c",
+    "url": "/portofolio/static/media/logo.126edd13.svg"
+  },
+  {
+    "revision": "9519c350ecd26aaccc5b61c2242580c9",
+    "url": "/portofolio/static/media/not-found.9519c350.svg"
+  },
+  {
+    "revision": "dbe14e5760985479c69beb26a9d0b1e6",
+    "url": "/portofolio/static/media/orange-box-diagonal.dbe14e57.svg"
+  },
+  {
+    "revision": "c6fe72b3bbbe4a0303d4a40e46d82f63",
+    "url": "/portofolio/static/media/orange-box.c6fe72b3.svg"
+  },
+  {
+    "revision": "37a0f688b7ee72c10c82b3d9cd6d686a",
+    "url": "/portofolio/static/media/phone.37a0f688.svg"
+  },
+  {
+    "revision": "8b90ed0e444dcf543644b9332bf5c34c",
+    "url": "/portofolio/static/media/website.8b90ed0e.svg"
+  },
+  {
+    "revision": "3841aef900b99a4d99037b5de4fe6cba",
+    "url": "/portofolio/static/media/white-circle.3841aef9.svg"
+  }
+]);
